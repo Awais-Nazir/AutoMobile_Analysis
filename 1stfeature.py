3bd7436f9987ce149159db61d425a8d69ec29533
Awais Nazir <cocaboya7@gmail.com>
2024-01-27 22:07:03 +0500

    Added again 1st feature

---
+++ 1stfeature.py
@@ -0,0 +1,14 @@
+# This is a sample Python code
+def greet(name):
+    print(f"Hello, {name}!")
+
+def add_numbers(a, b):
+    return a + b
+
+name = input("Enter your name: ")
+greet(name)
+
+num1 = 5
+num2 = 10
+result = add_numbers(num1, num2)
+print(f"The sum of {num1} and {num2} is {result}.")
